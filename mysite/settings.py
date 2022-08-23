9a8ff6368f2027647c4273b38d374636ce50aed5
BP <bpoldek@gmail.com>
2022-08-23 10:16:11 +0200

    new init

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 
+SECRET_KEY = os.environ['SECRET_KEY']
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y8ub15x+8jdj1l$=frd-m4or6mxrvdjq86n3ta5dv@i0l&=_@@'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
