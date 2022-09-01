2e6013ae56ee3dba329ccb5e40b94b03994d40ea
bp <bpoldek@gmail.com>
2022-09-01 13:14:55 +0200

    First css sheet

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -2,7 +2,8 @@
 
 from django.db import migrations, models
 import django.utils.timezone
-
+from django.conf import settings
+import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
@@ -15,10 +16,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Post',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=200)),
+                ('text', models.TextField()),
                 ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
-                ('publish_date', models.DateTimeField(blank=True, null=True)),
+                ('published_date', models.DateTimeField(blank=True, null=True)),
+                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
         ),
     ]
--- blog/models.py
+++ blog/models.py
@@ -5,10 +5,10 @@ class Post(models.Model):
     author: models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     created_date = models.DateTimeField(default=timezone.now)
-    publish_date = models.DateTimeField(blank=True,null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
-        self.publish_date = timezone.now()
+        self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
